1eeadb02e2f25e30a69a2f065f06698964763990
Conall Imlah <conallimlah@gmail.com>
2021-07-29 23:22:34 +0900

    tailwind test button

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -8,7 +8,7 @@ export const Login = () => {
   }
   return (
     <div>
-      <button type="submit" onClick={googleLogin}>Sign in with Google</button>
+      <button type="submit" onClick={googleLogin} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign in with Google</button>
     </div>
   )
 }
