82fbff1ea5d74692aaaa8245ff1bbae5eb11a95c
Conall Imlah <conallimlah@gmail.com>
2021-07-29 23:22:01 +0900

    add dotenv

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,6 +6,9 @@ import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
+require('dotenv').config();
+
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
