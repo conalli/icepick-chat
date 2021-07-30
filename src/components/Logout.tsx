d1393c0c6206f9c477e24a57bae56fd1f9bb78ad
Conall Imlah <conallimlah@gmail.com>
2021-07-30 23:36:50 +0900

    add routing and basic style to logout

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,9 +1,14 @@
 import { auth } from "../firebase/firebase";
+import { Redirect } from "react-router-dom";
 
 export const Logout = () => {
-  return auth.currentUser && (
+  const signOutHandler = () => {
+    auth.signOut();
+  }
+  // TODO: find out how to do this with history
+  return auth.currentUser ? (
     <div>
-      <button type="submit" onClick={() => auth.signOut()}>Sign out</button>
+      <button onClick={signOutHandler} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign out</button>
     </div>
-  )
+  ) : <Redirect to="/" />
 }
