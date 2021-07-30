50fd31aa4334870f931d3c62923c42d4c4bed4c9
Conall Imlah <conallimlah@gmail.com>
2021-07-30 23:18:56 +0900

    add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
 import './App.css';
 import firebase, { analytics, auth, db } from "./firebase/firebase"
 import { useAuthState } from "react-firebase-hooks/auth"
-import { useCollectionData } from "react-firebase-hooks/firestore"
+// import { useCollectionData } from "react-firebase-hooks/firestore"
+import { Header } from "./components/Header";
 import { Login } from './components/Login';
-import { Logout } from './components/Logout';
-import { TopicOption } from "./components/TopicOption";
+import { TopicOption, topicList } from "./components/TopicOption";
 
 function App() {
   const [user] = useAuthState(auth)
+
   
   return (
     <div className="App">
-      {user ? <TopicOption /> : <Login />}
-      <Logout />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            {user ?  <Redirect to="/icebreaker" />: <Login />}
+          </Route>
+          <Route exact path="/icebreaker">
+            <TopicOption />
+          </Route>
+          {topicList.map((topic, idx) => {
+            return (
+              <Route key={idx} exact path={`/icebreaker/${topic.name}`}>
+                <h1>{topic.name}</h1>
+              </Route>
+            )
+          })}
+        </Switch>
+        <Redirect to="/" />
+      </Router>
     </div>
   );
 }
