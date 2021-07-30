c013fa197aec75012d6f6b7c71a020c6ecbc8dcd
Conall Imlah <conallimlah@gmail.com>
2021-07-30 20:56:29 +0900

    update Login component

---
+++ src/components/Login.tsx
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+import { auth } from "../firebase/firebase";
+
+export const Login = () => {
+  const googleLogin = async () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    // TODO: decide which sign in style to use
+    // auth.signInWithRedirect(provider);
+    //   await auth.getRedirectResult()
+    try {
+      await auth.signInWithPopup(provider);
+    } catch(err) {
+      console.error(err);
+    }
+  }
+  return (
+    <div>
+      <button type="submit" onClick={googleLogin} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign in with Google</button>
+    </div>
+  )
+}
