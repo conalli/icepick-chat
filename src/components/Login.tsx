b6788ab581956601b40c14361aef5e245d761f64
Conall Imlah <conallimlah@gmail.com>
2021-07-31 10:24:28 +0900

    update login/logout text

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -16,7 +16,7 @@ export const Login = () => {
   }
   return (
     <div>
-      <button type="submit" onClick={googleLogin} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign in with Google</button>
+      <button onClick={googleLogin} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Log in with Google</button>
     </div>
   )
 }
--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -8,7 +8,7 @@ export const Logout = () => {
   // TODO: find out how to do this with history
   return auth.currentUser ? (
     <div>
-      <button onClick={signOutHandler} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign out</button>
+      <button onClick={signOutHandler} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Log out</button>
     </div>
   ) : <Redirect to="/" />
 }
