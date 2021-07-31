72978e04d14beabba2d2332ed358d83e117cbd3f
Conall Imlah <conallimlah@gmail.com>
2021-07-31 12:48:20 +0900

    add app colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ip-black": "#0B0C10",
+        "ip-darkgrey": "#1F2833",
+        "ip-grey": "#C5C6C7",
+        "ip-primary": "#66FCF1",
+        "ip-secondary": "#45A29E",
+      },
+    },
   },
   variants: {
     extend: {},
