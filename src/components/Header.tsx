e3edbc777c8b194a18eb910104e80e34203ce05a
Conall Imlah <conallimlah@gmail.com>
2021-08-01 15:04:58 +0900

    temp-fix better styling for logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
+import { auth } from "../firebase/firebase";
 import { Logout } from './Logout';
 export const Header = () => {
   return (
-    <header>
-      <h1>Icepick</h1>
-      <Logout />
+    <header className="grid grid-cols-2 mx-auto p-12 items-center">
+      <h1 className="self-start font-sans text-4xl col-span-1 place-self-start">
+        <span className="logo-ice text-white">Ice</span>
+        <span className="logo-pick text-ip-primary">pick</span>
+        <span className="logo-period text-ip-secondary">.</span>
+      </h1>
+      {auth.currentUser && <Logout />}
     </header>
   )
 }
